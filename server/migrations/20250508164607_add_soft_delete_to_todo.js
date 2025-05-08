bd5ae010d8f00598c888ddf93afc7eda085df929
João Seixas <joaoseixas88@gmail.com>
2025-05-08 14:06:42 -0300

    feat: add soft delete to todos and users

---
+++ server/migrations/20250508164607_add_soft_delete_to_todo.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("todos", function (table) {
+    table.timestamp("deleted_at").defaultTo(null);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("todos", function (table) {
+    table.dropColumn("deleted_at");
+  });
+};
